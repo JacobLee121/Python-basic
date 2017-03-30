fdd8e73f63209b1305163ee6ac27af739322ffbb
JacobLee121 <JacobLee121@users.noreply.github.com>
2017-03-30 22:35:03 +0800

    Create input.py

---
+++ input.py
@@ -0,0 +1,19 @@
+>>> nn=input()
+124
+>>> nn
+'124'
+
+>>> nn=input('dfd')
+dfd124
+>>> nn
+'124'
+>>> nn=input("dfs")
+dfs23
+>>> nn
+'23'
+>>> nn=input("dddd\n")
+dddd
+1234
+>>> nn
+'1234'
+#python 换行符\n,读入的是字符串
